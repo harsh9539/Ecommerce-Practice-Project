781d9ac351100766ee0a5fb35dd4f028d8a99e14
harsh9539 <goyalh862@gmail.com>
2022-10-27 13:02:33 +0530

    register and login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 
 // import Home from './pages/Home';
-import Product  from './pages/Product';
+// import Product  from './pages/Product';
+import Login from './pages/Login';
+import Register from './pages/Register';
 // import ProductList from './pages/ProductList';
 
 function App() {
   return (
     <div className="App">
-      <Product/>
+      <Login/>
     </div>
   );
 }
