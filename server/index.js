932dd9089bcd20aa7242f467dc077bfc334f8239
harsh9539 <goyalh862@gmail.com>
2022-10-27 17:40:42 +0530

    backend setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-console.log("yeee")
+import dotenv from 'dotenv'
+import express from "express";
+import mongoose from "mongoose";
+import userRoute from './routes/user.js';
+
+
+const app = express();
+dotenv.config();
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URL).then(() => { console.log("DB connection successful") }).catch((err)=>console.log(err));
+
+
+// API
+app.use("/api/users",userRoute)
+
+// PORT
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Backend server is running on port ${process.env.PORT}`);
+})
---
+++ server/routes/auth.js
@@ -0,0 +1,12 @@
+import express  from "express";
+const router = express.Router();
+
+
+
+//Register
+router.post("/register",(req,res)=>{
+
+})
+
+
+export default router;
